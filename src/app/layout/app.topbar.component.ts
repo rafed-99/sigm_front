aa382c90f16add324678cb59802d014c460fbee4
rafed-99 <73440128+rafed-99@users.noreply.github.com>
2023-05-22 10:22:08 +0100

    dark mode button , cleaning sidebar

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -19,9 +19,12 @@ export class AppTopBarComponent {
 
     profile :string = ""
 
+    darkMode ?:boolean =false;
+
     constructor(public layoutService: LayoutService , private router:Router) { }
 
     ngOnInit() : void{
+        
         
         
         this.profile = sessionStorage.getItem("profile")!;
@@ -32,5 +35,45 @@ export class AppTopBarComponent {
     Logout(){
         sessionStorage.clear();
         this.router.navigate(['/auth/login']);
+    }
+
+    changeTheme(theme: string, colorScheme: string) {
+        const themeLink = <HTMLLinkElement>document.getElementById('theme-css');
+        const newHref = themeLink.getAttribute('href')!.replace(this.layoutService.config.theme, theme);
+        this.layoutService.config.colorScheme
+        this.replaceThemeLink(newHref, () => {
+            this.layoutService.config.theme = theme;
+            this.layoutService.config.colorScheme = colorScheme;
+            this.layoutService.onConfigUpdate();
+        });
+    }
+
+    replaceThemeLink(href: string, onComplete: Function) {
+        const id = 'theme-css';
+        const themeLink = <HTMLLinkElement>document.getElementById('theme-css');
+        const cloneLinkElement = <HTMLLinkElement>themeLink.cloneNode(true);
+
+        cloneLinkElement.setAttribute('href', href);
+        cloneLinkElement.setAttribute('id', id + '-clone');
+
+        themeLink.parentNode!.insertBefore(cloneLinkElement, themeLink.nextSibling);
+
+        cloneLinkElement.addEventListener('load', () => {
+            themeLink.remove();
+            cloneLinkElement.setAttribute('id', id);
+            onComplete();
+        });
+    }
+
+    buttonClicked(){
+    
+    if(!this.darkMode){
+        this.changeTheme('vela-blue', 'dark')
+        
+    }
+    else{
+        this.changeTheme('lara-light-indigo', 'light')
+    }
+    this.darkMode = !this.darkMode;
     }
 }
